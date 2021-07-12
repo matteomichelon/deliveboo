a8cdda492c3fbce1ee9f8d0ac19060a3dc88c75c
CATALIN ZAHARIEA <zahariea.c@gmail.com>
2021-07-12 19:32:04 +0200

    Added "new_product" button to user nav bar.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,9 @@
                                 </li>
                             @endif
                         @else
+
+                            <a href="{{route('user.products.create')}}">Aggiungi un nuovo prodotto</a>
+                            
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
