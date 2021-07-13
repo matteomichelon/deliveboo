04209e0bc09077d7d10ab8ceb2e11b3d04e13a44
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-07-13 16:42:50 +0200

    modificato header e sass header, visualizzati buttons authentication

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,33 +1,77 @@
 <header>
-        <nav class="navbar navbar-expand-md navbar-light shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{asset('img/logo-test-2.png')}}" alt="logo">
-                    <!--{{ config('app.name', 'Laravel') }}-->
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <nav>
+            <div class="container_large">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                <div class="nav-left">
 
-                    </ul>
+                    <div class="logo">
+
+                        <a href="{{ url('/') }}">
+                            <img src="{{asset('img/logo-test-2.png')}}" alt="logo">
+                        </a>
+                    </div>
+                    
+                </div>
+
+                <div class="nav-right">
+
+                    <div class="login_register">
+                        <ul>
+                            @guest
+                                <li>
+                                    <a class="btn-nav" href="{{ route('login') }}">Login</a>
+                                </li>
+                                @if (Route::has('register'))
+                                    <li>
+                                        <a class="btn-nav" href="{{ route('register') }}">Registrati</a>
+                                    </li>
+                                @endif
+                                @else
+
+
+                                <a href="{{route('admin.products.create')}}">Aggiungi un nuovo prodotto</a>
+                                
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
+                                    </a>
+
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
+                        
+                    </div>
+                </div>
+                
+            
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent"> -->
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
+                    <!-- <ul>
+                        Authentication Links 
                         @guest
-                            <li class="nav-item">
+                            <li>
                                 <a class="btn-nav" href="{{ route('login') }}">Login</a>
                             </li>
                             @if (Route::has('register'))
-                                <li class="nav-item">
+                                <li>
                                     <a class="btn-nav" href="{{ route('register') }}">Registrati</a>
                                 </li>
                             @endif
-                        @else
+                            @else
+
 
                             <a href="{{route('admin.products.create')}}">Aggiungi un nuovo prodotto</a>
                             
@@ -49,8 +93,8 @@
                                 </div>
                             </li>
                         @endguest
-                    </ul>
-                </div>
+                    </ul> 
+                </div>  -->
             </div>
         </nav>
 </header>
