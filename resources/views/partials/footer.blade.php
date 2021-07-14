2d60e6e42f4f4acd6ade60029945b00d1f6b1064
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-07-14 16:58:40 +0200

    aggiunto footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,122 @@
+<!-- FOOTER -->
+<footer>
+
+    <div class="container_medium padding-section">
+
+        <div class="container-box">
+            
+            <!--menu-aiuto-->
+            <ul class="menu-footer">
+
+                <li>
+                    <h5>aiuto</h5>
+                </li>
+
+                <li>
+                    <a href="#">Contatti</a>
+                </li>
+
+                <li>
+                    <a href="#">FAQ</a>
+                </li>
+
+                <li>
+                    <a href="#">Tipi di cucina</a>
+                </li>
+
+                <li>
+                    <a href="#">Mappa del sito</a>
+                </li>
+
+            </ul>
+            <!--end menu-aiuto-->
+
+            <!--menu-note legali-->
+            <ul class="menu-footer">
+
+                <li>
+                    <h5>note legali</h5>
+                </li>
+
+                <li>
+                    <a href="#">Termini & Condizioni</a>
+                </li>
+
+                <li>
+                    <a href="#">Informativa sulla Privacy</a>
+                </li>
+
+                <li>
+                    <a href="#">Cookies</a>
+                </li>
+
+            </ul>
+            <!--end menu-note legali-->
+
+            <!--menu-contatti-->
+            <ul class="menu-footer">
+
+                <li>
+                    <h5>contatti</h5>
+                </li>
+
+                <li>
+                    <a href="#">Via XXIV Maggio 7 Roma, 00127</a>
+                </li>
+
+                <li>
+                    <a href="#">06-9395720 | 9a.m-5p.m (Lun - Ven)</a>
+                </li>
+
+                <li>
+                    <a href="#">support@deliveboo.com</a>
+                </li>
+
+            </ul>
+            <!--end menu-contatti-->
+
+            <!--menu-social-->
+            <ul class="menu-footer">
+
+                <li>
+                    <h5>social</h5>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <i class="fab fa-facebook-square"></i>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <i class="fab fa-linkedin-in"></i>
+                    </a>
+                </li>
+
+            </ul>
+            <!--end menu-social-->
+
+        </div>
+         
+    </div>
+
+    <!--copyright-->
+    <div class="copyright">
+        © 2021 Deliveboo
+    </div>
+    <!--end copyright-->
+
+</footer>
