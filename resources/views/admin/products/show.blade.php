79800771b8a0b21052c4cafef72b959b3ce8af8e
Mario Koco <mario.koco92@gmail.com>
2021-07-14 15:44:21 +0200

    aggiunta immagine nei dettagli del prodotto

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -5,6 +5,9 @@
     <div class="jumbotron">
         <h1 class="display-4">{{$product['name']}}</h1>
         <h6 class="card-subtitle mb-2 text-muted">€{{ number_format($product->price,2) }}</h6>
+        <div class="mt-2 mb-2">
+            <img src="{{ asset('storage/' . $product->cover) }}" alt="{{ $product->name }}">
+        </div>
         <p class="lead">{{$product['description']}}</p>
         <a class="btn btn-primary btn-lg" href="{{route('admin.products.index')}}" role="button">Torna indietro</a>
         <a class="btn btn-secondary btn-lg" href="{{route('admin.products.edit', ['product' => $product->id])}}" role="button">Modifica prodotto</a>
