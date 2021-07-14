dc2e43201946e88686793fdbc2417d9b17af90a1
Mario Koco <mario.koco92@gmail.com>
2021-07-14 12:59:00 +0200

    fix visualizzazione 2 decimali per i prezzi

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -18,7 +18,7 @@
 
                 <div class="card-body">
                     <h5 class="card-title">{{$product->name}}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">€{{$product->price}}</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">€{{ number_format($product->price,2) }}</h6>
                     <p class="card-text">{{$product->description}}</p>
 
                     {{-- USER TOOLS --}}
--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="jumbotron">
         <h1 class="display-4">{{$product['name']}}</h1>
-        <h6 class="card-subtitle mb-2 text-muted">€{{$product->price}}</h6>
+        <h6 class="card-subtitle mb-2 text-muted">€{{ number_format($product->price,2) }}</h6>
         <p class="lead">{{$product['description']}}</p>
         <a class="btn btn-primary btn-lg" href="{{route('admin.products.index')}}" role="button">Torna indietro</a>
         <a class="btn btn-secondary btn-lg" href="{{route('admin.products.edit', ['product' => $product->id])}}" role="button">Modifica prodotto</a>
