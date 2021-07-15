37135698a1a41f0a2cab9c34e472e037b19a580c
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-07-15 15:24:10 +0200

    Cambiati path immagini categorie in .jpg

--- public/js/app.js
+++ public/js/app.js
@@ -37410,13 +37410,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
 __webpack_require__(/*! C:\Users\usuario\Desktop\deliveboo\resources\js\app.js */"./resources/js/app.js");
 module.exports = __webpack_require__(/*! C:\Users\usuario\Desktop\deliveboo\resources\sass\app.scss */"./resources/sass/app.scss");
-=======
-__webpack_require__(/*! /Users/catalinzahariea/Desktop/Boolean/Deliveboo/deliveboo/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/catalinzahariea/Desktop/Boolean/Deliveboo/deliveboo/resources/sass/app.scss */"./resources/sass/app.scss");
->>>>>>> faa18a1e52008cf78fc70d49b0cd43fe56c21ac3
 
 
 /***/ })
