446c82136464bc4608d707e4dd4db21ce83c143a
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-07-16 17:32:25 +0200

    Sistemata view index dei prodotti (admin)

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,28 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="d-flex flex-wrap">
+<div class="container_large">
+    <div class="container-box d-flex flex-wrap">
         
         @foreach ($products as $product)
 
             {{-- Product Card --}}
-            <div class="card" style="width: 18rem;">
+            <div class="card-product">
                 
-                {{-- 
-                    If the product has a cover image set it will display it, otherwise
-                    it will display a standard image.    
-                --}}
-                @if (isset($product->cover) )
-                    <img src="{{$product->cover}}" class="card-img-top" alt="{{$product->name}}">
-                @else
-                    <img src="https://picsum.photos/300" class="card-img-top" alt="{{$product->name}}">
-                @endif
+                
+                <div class="container-image">
+                    {{--  If the product has a cover image set it will display it, otherwise
+                    it will display a standard image.    --}}
+                    
+                    @if (isset($product->cover) )
+                        <img src="{{$product->cover}}" class="card-img-top" alt="{{$product->name}}">
+                    @else
+                        <img src="https://picsum.photos/300" class="card-img-top" alt="{{$product->name}}">
+                    @endif
+                </div>
 
                 <div class="card-body">
                     <h5 class="card-title">{{$product->name}}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">€{{ number_format($product->price,2) }}</h6>
-                    <p class="card-text">{{$product->description}}</p>
+
+                    @if ($product->description)
+                        <p class="card-text">{{substr($product->description, 0, 70)}}...</p>
+                    @endif
 
                     {{-- USER TOOLS --}}
                     <a href="{{route('admin.products.show', ['product' => $product->id])}}" class="btn btn-success">Visualizza</a>
