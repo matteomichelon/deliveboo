a0c983780da968a8d11cc054ea12b024ebdd4687
Mario Koco <mario.koco92@gmail.com>
2021-07-16 16:37:25 +0200

    aggiunti altri prodotti

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -549,6 +549,286 @@ class ProductsTableSeeder extends Seeder
                 'cover' => 'https://nishkitchen.com/wp-content/uploads/2016/03/coconut-burfi-1B-480x480.jpg',
                 'user_id' => 13
             ],
+            [
+                'name' => 'Tagliata di angus',
+                'description' => 'Ingredienti: Controfiletto di Angus',
+                'price' => 13.00,
+                'visibility' => 1,
+                'cover' => 'https://www.ristorantecotcos.it/media/k2/items/cache/fe4bbe81600a40063594e597e00eb05b_L.jpg',
+                'user_id' => 14
+            ],
+            [
+                'name' => 'Tagliere si salumi e formaggi',
+                'description' => 'Ingredienti: Salumi e formaggi della zona, in base alla disponibilità del giorno.',
+                'price' => 9.00,
+                'visibility' => 1,
+                'cover' => 'https://media-cdn.tripadvisor.com/media/photo-s/0e/03/e8/6c/tagliere-di-salumi-e.jpg',
+                'user_id' => 14
+            ],
+            [
+                'name' => 'Risotto alla milanese',
+                'description' => 'Ingredienti: Riso carnaroli, zafferano, burro, grana.',
+                'price' => 9.00,
+                'visibility' => 1,
+                'cover' => 'https://www.conad.it/content/dam/conad/conad-consumer/ricette/DayOne%20Ricette/S&D/S&D_RisottoMilanese.jpg',
+                'user_id' => 14
+            ],
+            [
+                'name' => 'Caprese di bufala',
+                'description' => 'Ingredienti: Mozzarella di bufala D.O.P., pomodoro cuore di bue, basilico, olio evo.',
+                'price' => 8.00,
+                'visibility' => 1,
+                'cover' => 'https://img1.spiiky.com/events/10/80045_0_59c6ba957ab74990ad9ac1da02bde605.jpg?format=webp&quality=80&scale=both&mode=crop&maxwidth=735&maxheight=427&bgcolor=F6F6F6&crop=(3,51,760,490)',
+                'user_id' => 14
+            ],
+            [
+                'name' => 'Insalata di pollo',
+                'description' => 'Ingredienti: Lattuga, pollo grigliato, capperi, olive.',
+                'price' => 7.00,
+                'visibility' => 1,
+                'cover' => 'https://www.cucchiaio.it/content/cucchiaio/it/ricette/2019/06/insalata-di-pollo/jcr:content/header-par/image-single.img10.jpg/1560261890876.jpg',
+                'user_id' => 14
+            ],
+            [
+                'name' => 'French toast',
+                'description' => 'Ingredienti: Pane in cassetta, edamer, cotto, uovo occhio di bue, pepe.',
+                'price' => 5.00,
+                'visibility' => 1,
+                'cover' => 'https://coopmaster.it/blog/wp-content/uploads/2021/01/french-toast.jpg',
+                'user_id' => 15
+            ],
+            [
+                'name' => 'Classico',
+                'description' => 'Ingredienti: Pane in cassetta, edamer, cotto.',
+                'price' => 3.50,
+                'visibility' => 1,
+                'cover' => 'https://www.negroni.com/sites/negroni.com/files/styles/scale__1440_x_1440_/public/storiatoast.jpg?itok=NqRQjOjM',
+                'user_id' => 15
+            ],
+            [
+                'name' => 'Gorgo toast',
+                'description' => 'Ingredienti: Pane in cassetta, gorgonzola dolce, confettura di pere fatta in casa, nocciole tostate.',
+                'price' => 4.00,
+                'visibility' => 1,
+                'cover' => 'https://www.igorgorgonzola.com/sistemaNews/newsFoto/videoricette/ricetta-toast-pere-gorgonzola-dolce.jpg',
+                'user_id' => 15
+            ],
+            [
+                'name' => 'Toast veggie',
+                'description' => 'Ingredienti: Pane in cassetta, uovo in camicia, pesto di avocado e basilico.',
+                'price' => 4.50,
+                'visibility' => 1,
+                'cover' => 'https://static.cookist.it/wp-content/uploads/sites/21/2019/11/toast-con-avocado-e-uova-in-camicia-1200x675.jpg',
+                'user_id' => 15
+            ],
+            [
+                'name' => 'Toast gourmet',
+                'description' => 'Ingredienti: Pane in cassetta, brie, mortadella, pesto di zucchine e pistacchi.',
+                'price' => 7.00,
+                'visibility' => 1,
+                'cover' => 'https://capatoast.it/blog/images/articoli/Ricette/Buongustaio/anteprima.jpg',
+                'user_id' => 15
+            ],
+            [
+                'name' => 'Tempura di verdure',
+                'description' => 'Ingredienti: Verdure del giorno in pastella.',
+                'price' => 3.20,
+                'visibility' => 1,
+                'cover' => 'https://primochef.it/wp-content/uploads/2017/02/SH_tempura_verdure.jpg',
+                'user_id' => 16
+            ],
+            [
+                'name' => 'Pollo fritto alla cinese',
+                'description' => 'Ingredienti: Bocconcini di pollo fritto in pastella.',
+                'price' => 3.40,
+                'visibility' => 1,
+                'cover' => 'https://blog.giallozafferano.it/delizieinpentola/wp-content/uploads/2021/01/Pollo-fritto.jpg',
+                'user_id' => 16
+            ],
+            [
+                'name' => 'Gnocchi di riso',
+                'description' => 'Ingredienti: Gnocchi di riso, carote, funghi, zucchine.',
+                'price' => 4.00,
+                'visibility' => 1,
+                'cover' => 'https://m.my-personaltrainer.it/images/ricette/791/gnocchi-riso-verdure.jpg',
+                'user_id' => 16
+            ],
+            [
+                'name' => 'Pollo con ananas',
+                'description' => 'Ingredienti: Carne di pollo, ananas.',
+                'price' => 5.00,
+                'visibility' => 1,
+                'cover' => 'https://www.misya.info/wp-content/uploads/2018/02/pollo-all-ananas.jpg',
+                'user_id' => 16
+            ],
+            [
+                'name' => 'Manzo al curry',
+                'description' => 'Ingredienti: Carne di manzo, patate, curry.',
+                'price' => 5.50,
+                'visibility' => 1,
+                'cover' => 'http://www.kung-food.it/wp-content/uploads/2014/11/spezzatino-di-vitello-al-curry.jpg',
+                'user_id' => 16
+            ],
+            [
+                'name' => 'Naan',
+                'description' => 'Ingredienti: Focaccia classica cotta nel forno tandoor.',
+                'price' => 3.00,
+                'visibility' => 1,
+                'cover' => 'https://rasamalaysia.com/wp-content/uploads/2019/03/naan2.jpg',
+                'user_id' => 17
+            ],
+            [
+                'name' => 'Pollo zafferano',
+                'description' => 'Ingredienti: Pollo in pezzi, zafferano e spezie.',
+                'price' => 5.00,
+                'visibility' => 1,
+                'cover' => 'https://www.lacucinaimperfetta.com/wp-content/uploads/2015/10/Pollo-allo-zafferano.jpg',
+                'user_id' => 17
+            ],
+            [
+                'name' => 'Pollo tandori',
+                'description' => 'Ingredienti: Fusi di pollo, yogurt, tandori e zenzero.',
+                'price' => 5.50,
+                'visibility' => 1,
+                'cover' => 'https://www.cucchiaio.it/content/cucchiaio/it/ricette/2010/06/ricetta-pollo-tandoori/_jcr_content/header-par/image_single.img10.jpg/1452273814970.jpg',
+                'user_id' => 17
+            ],
+            [
+                'name' => 'Lassi',
+                'description' => 'Ingredienti: Yogurt, acqua, semi di cumino e cardamomo.',
+                'price' => 3.00,
+                'visibility' => 1,
+                'cover' => 'https://www.greenme.it/wp-content/uploads/2016/07/lassi_indiano.jpg',
+                'user_id' => 17
+            ],
+            [
+                'name' => 'Bhel Puri',
+                'description' => 'Ingredienti: Riso soffiato, verdure saltate, salsa piccante al tamarindo.',
+                'price' => 8.00,
+                'visibility' => 1,
+                'cover' => 'https://rasamalaysia.com/wp-content/uploads/2019/03/naan2.jpg',
+                'user_id' => 17
+            ],
+            [
+                'name' => 'Onion rings',
+                'description' => 'Ingredienti: Anelli di cipolla impanati.',
+                'price' => 6.00,
+                'visibility' => 1,
+                'cover' => 'https://www.nuevomundopub.com/PubFoggia/wp-content/uploads/2020/04/onion-rings-1.jpg',
+                'user_id' => 18
+            ],
+            [
+                'name' => 'Cheese fried',
+                'description' => 'Ingredienti: Anelli di cipolla impanati.',
+                'price' => 8.00,
+                'visibility' => 1,
+                'cover' => 'https://food-fanatic-res.cloudinary.com/iu/s--m7eeADvN--/c_thumb,f_auto,g_auto,h_1200,q_auto,w_1200/v1537755904/fried-cheese-balls-photo',
+                'user_id' => 18
+            ],
+            [
+                'name' => 'Hamurger del diablo',
+                'description' => 'Ingredienti: Bun con sesamo, doppia carne di bovino da 250g, doppio cheddar, doppio bacon, salsa bbq.',
+                'price' => 13.00,
+                'visibility' => 1,
+                'cover' => 'https://media-cdn.tripadvisor.com/media/photo-s/12/3c/f7/a1/er-grande-doppio-manzo.jpg',
+                'user_id' => 18
+            ],
+            [
+                'name' => 'King burger',
+                'description' => 'Ingredienti: Bun con sesamo, doppia carne di cavallo da 300g, cheddar, bacon insalata, pomodoro, accompagnata da patatine fritte.',
+                'price' => 18.00,
+                'visibility' => 1,
+                'cover' => 'https://www.piaceriditalia.it/wp/wp-content/uploads/2019/05/AdobeStock_157743662-1500x1069.jpeg',
+                'user_id' => 18
+            ],
+            [
+                'name' => 'Beef fajitas',
+                'description' => 'Ingredienti: Straccetti di manzo saltati in padella con salsa di soia, peperoni e cipolla, accompagnate da calde tortillas.',
+                'price' => 26.00,
+                'visibility' => 1,
+                'cover' => 'https://www.diablosaloon.com/wp-content/uploads/2020/12/fajits-pavia-1.jpg',
+                'user_id' => 18
+            ],
+            [
+                'name' => 'Texas salad',
+                'description' => 'Ingredienti: Insalata verde, scaglie di grana, pollo, fagioli, pomodori, mais.',
+                'price' => 10.00,
+                'visibility' => 1,
+                'cover' => 'https://images.otstatic.com/prod/25954352/1/large.jpg',
+                'user_id' => 19
+            ],
+            [
+                'name' => 'Chicken caesar salad',
+                'description' => 'Ingredienti: Insalata verde, scaglie di grana, crostini di pane, acciughe, pollo, salsa dressing.',
+                'price' => 8.00,
+                'visibility' => 1,
+                'cover' => 'https://i2.wp.com/mygorgeousrecipes.com/wp-content/uploads/2019/01/Grilled-Chicken-Caesar-Salad-with-Heatlhy-Yogurt-Dressing-j6.jpg',
+                'user_id' => 19
+            ],
+            [
+                'name' => 'Rib eye',
+                'description' => 'Ingredienti: Tenero controfiletto di Black Angus irlandese senza osso 370g. circa, servito con patate al forno, quesadillas e insalata mista.',
+                'price' => 18.00,
+                'visibility' => 1,
+                'cover' => 'https://s3.amazonaws.com/thegrillfather/wp-content/uploads/2019/06/hawaiian-rib-eye-steak-1.jpeg',
+                'user_id' => 19
+            ],
+            [
+                'name' => 'Churrasco',
+                'description' => 'Ingredienti: Fettine di manzo irlandese (350gr circa) speziato servito con patate fritte e quesadillas.',
+                'price' => 14.00,
+                'visibility' => 1,
+                'cover' => 'http://www.ristorantechacarero.it/uploads/immagini_pagine/piatti/grigliata-gaucha-modific.jpg',
+                'user_id' => 19
+            ],
+            [
+                'name' => 'Apple pie',
+                'description' => 'Ingredienti: Classica torta di mele all\'americana.',
+                'price' => 5.00,
+                'visibility' => 1,
+                'cover' => 'https://wips.plug.it/cips/buonissimo.org/cms/2012/02/apple-pie.jpg?w=713&a=c&h=407',
+                'user_id' => 19
+            ],
+            [
+                'name' => 'Burritos',
+                'description' => 'Ingredienti: Maiale stufato, riso, mais, fagioli messicani.',
+                'price' => 7.00,
+                'visibility' => 1,
+                'cover' => 'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/DAC7A66A-BB1B-49AD-95BF-83EF881DEDAE/Derivates/1abc774a-220c-4d98-912a-2ad56675b6a6.jpg',
+                'user_id' => 20
+            ],
+            [
+                'name' => 'Tacos di pollo',
+                'description' => 'Ingredienti: Pollo grigliato, guacamole tortilla, tabasco.',
+                'price' => 5.00,
+                'visibility' => 1,
+                'cover' => 'https://www.soniaperonaci.it/wp-content/uploads/2021/01/Tacos-con-pollo-e-guacamole-quadrata-anteprima-960x960.jpg',
+                'user_id' => 20
+            ],
+            [
+                'name' => 'Chili',
+                'description' => 'Classico chili di carne di manzo alla texana.',
+                'price' => 7.00,
+                'visibility' => 1,
+                'cover' => 'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/4AABF349-67E4-44C5-9C07-153CF5897C3F/Derivates/4F81BC9B-DAD7-4887-B12B-0EC0C1935906.jpg',
+                'user_id' => 20
+            ],
+            [
+                'name' => 'Nacho con chili',
+                'description' => 'Ingredienti: Nachos con chili alla messicana, guacamole, panna acida.',
+                'price' => 9.00,
+                'visibility' => 1,
+                'cover' => 'https://juegoscocinarpasteleria.org/wp-content/uploads/2020/02/Los-mejores-nachos.jpg',
+                'user_id' => 20
+            ],
+            [
+                'name' => 'Quesadilla',
+                'description' => 'Ingredienti: Quesadilla ripiena di chili e formaggio cheddar.',
+                'price' => 5.00,
+                'visibility' => 1,
+                'cover' => 'https://images.ctfassets.net/2x1b56neok6m/it-media-3402-asset/cc30deb6cd125328c213169aa955d426/shutterstock_531876007.jpg?w=799&q=50',
+                'user_id' => 20
+            ],
 
 
 
