a392f083a4b98d9fb897aab5bfc82a7c5e1057cc
Mario Koco <mario.koco92@gmail.com>
2021-07-20 10:56:20 +0200

    rimosso zaninotto e inserito faker php, inseriti ordini casuali nel database

--- database/seeds/OrdersTableSeeder.php
+++ database/seeds/OrdersTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Order;
+use Faker\Generator as Faker;
 
 class OrdersTableSeeder extends Seeder
 {
@@ -10,62 +11,24 @@ class OrdersTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+
+    public function run(Faker $faker)
     {
-        $orders = [
-            [
-                'code' => 'FNO68143TD',
-                'price' => 20.20,
-                'status' => 1,
-                'date' => '2021-07-12 09:17:35',
-                'address' => 'Via Manzoni 25',
-                'name' => 'Mario',
-                'surname' => 'Bianchi',
-                'email' => 'mariobianchi@alice.it',
-                'notes' => 'Primo piano',
-                'telephone_number' => '3401716567'
-            ],
-            [
-                'code' => 'FNO68143TD',
-                'price' => 18.50,
-                'status' => 1,
-                'date' => '2021-07-15 09:17:35',
-                'address' => 'Via Garibaldi 44',
-                'name' => 'Valentina',
-                'surname' => 'Verdi',
-                'email' => 'valentinaverdi@gmail.com',
-                'notes' => '',
-                'telephone_number' => '3464916567'
-            ],
-            [
-                'code' => 'FNO68143TD',
-                'price' => 10.55,
-                'status' => 1,
-                'date' => '2021-07-10 09:17:35',
-                'address' => 'Via Mazzini 108',
-                'name' => 'Giuseppe',
-                'surname' => 'Bianchi',
-                'email' => 'giuseppebianchi@alice.it',
-                'notes' => 'Terzo piano',
-                'telephone_number' => '3401716123'
-            ]
-        ];
 
-        foreach($orders as $order) {
+        for($i = 0; $i < 50; $i++){
             $new_order = new Order;
-
-            $new_order->code = $order['code'];
-            $new_order->price = $order['price'];
-            $new_order->status = $order['status'];
-            $new_order->date = $order['date'];
-            $new_order->address = $order['address'];
-            $new_order->name = $order['name'];
-            $new_order->surname = $order['surname'];
-            $new_order->email = $order['email'];
-            $new_order->notes = $order['notes'];
-            $new_order->telephone_number = $order['telephone_number'];
-
+            $new_order->code = $faker->regexify('[A-Z]{5}[0-4]{5}');
+            $new_order->price = $faker->randomFloat(2, 3, 100);
+            $new_order->status = '1';
+            $new_order->date = $faker->dateTimeBetween('-2 month', '+1 month');
+            $new_order->address = $faker->address();
+            $new_order->name = $faker->firstName();
+            $new_order->surname = $faker->lastName();
+            $new_order->email = $faker->email();
+            $new_order->notes = $faker->text(50);
+            $new_order->telephone_number = $faker->phoneNumber();
             $new_order->save();
+            
         }
     }
 }
