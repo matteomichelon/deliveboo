a2a595a23e235db86a152ee7199e82a00f245359
Matteo Michelon <michelon.matteo89@gmail.com>
2021-07-21 16:06:18 +0200

    test vue chart.js

--- resources/views/admin/stats.blade.php
+++ resources/views/admin/stats.blade.php
@@ -3,26 +3,52 @@
 @section('content')
     <h1>Sono la home delle statistiche</h1>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 offset-md-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
-                    <div class="panel-body">
-                        <canvas id="canvas" height="280" width="600"></canvas>
+    <div id="root">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 offset-md-1">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Dashboard</div>
+                        <div class="panel-body">
+                            <canvas id="canvas" height="280" width="600"></canvas>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
+    
+
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
 
     {{-- Start Script --}}
     <script>
-  
+        new Vue({
+            el: '#root',
+            data: {
+                orders: [],
+                countOrder: [],
+            },
+            mounted() {
+                axios
+                    .get('api/orders', {
+                        params: {
+                            id: {{ $user->id }}
+                        }
+                    })
+                    .then((result) => {
+                        this.orders = result.data.orders;
+                    })
+            }
+        }
+
+        })
+
     </script>
+
+
     {{-- End Script --}}
 
 @endsection
