cb6fa617a52bbbb2f9333914ae5fc9cb5539a7f0
Mario Koco <mario.koco92@gmail.com>
2021-07-22 17:11:52 +0200

    inserita joi per i prodotti

--- app/Http/Controllers/User/StatsController.php
+++ app/Http/Controllers/User/StatsController.php
@@ -5,9 +5,21 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class StatsController extends Controller
 {
+    public function joinTables()
+    {
+        /* Join Sql per gli ricavare Utenti/Ordini */
+        $order_schema = DB::table('order_product')
+                    ->join('products', 'product_id', '=', 'products.id')
+                    ->join('orders', 'order_id', '=', 'orders.id')
+                    ->join('users', 'user_id', '=', 'users.id')
+                    ->get();
+                    dd($order_schema);
+    }
+
     public function orders($id)
     {   
        $user = Auth::user();
@@ -18,4 +30,5 @@ class StatsController extends Controller
 
         return view('admin.stats', $data);
     }
+
 }
