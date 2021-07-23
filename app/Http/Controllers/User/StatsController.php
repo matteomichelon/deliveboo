1cc5a740989f35d497128c3d32f2ec28e41d614a
Matteo Michelon <michelon.matteo89@gmail.com>
2021-07-23 17:07:28 +0200

    Aggiornato le statistiche con aggiunta grafico mese e anno

--- app/Http/Controllers/User/StatsController.php
+++ app/Http/Controllers/User/StatsController.php
@@ -28,9 +28,9 @@ class StatsController extends Controller
 
         $year_array = [];
 
-
         $year_order_array = [];
-        $month_array = [
+
+        /* $month_array = [
             'gennaio',
             'febbraio',
             'marzo',
@@ -43,58 +43,68 @@ class StatsController extends Controller
             'ottobre',
             'novembre',
             'dicembre'
-        ];
+        ]; */
+
+        $month_array = [];
+
         $month_order_array = [];
 
-        /* foreach ($array as $value) {
-            dump($value->date);
-        } */
-                
-        /* Array suddiviso per anni e mesi */
         foreach ($array as $value) {
             $date = $value->date;
             $year_date = '';
             $month_date = '';
-            $id_order =  $value->order_id;            
+            
+            if ('2021' == date('Y', strtotime($date))) {
+                $month_date = date('F', strtotime($date));
+                array_push($month_array, $month_date);
+            }
+        }
+                
+        /* Array suddiviso per anni */
+        foreach ($array as $value) {
+            $date = $value->date;
+            $year_date = '';
             
             if ($date) {
                 $year_date = date('Y', strtotime($date));
-                //$month_date = date('F', strtotime($date));
-                array_push($year_array,$year_date);
-                //array_push($data_array, $year_date, $month_date);
+                array_push($year_array, $year_date);
             }
         }
 
-        usort($year_array, fn($a, $b) => strtotime($a) - strtotime($b));
+        usort($year_array, fn ($a, $b) => strtotime($a) - strtotime($b));
 
-        /* TODO: contare ordini per anno */
+        /* Funzioni riferite ad anno */
         $year_unique = array_count_values($year_array);
 
         $year_order_array = array_values($year_unique);
 
-        dump($year_order_array);
-
         $year_array = array_unique($year_array);
-       
 
-        dd($year_array);
+        $year_array = array_values($year_array);
 
-        
+        /* Funzioni riferite ai mesi del 2021 */
+        $month_unique = array_count_values($month_array);
 
-        
-        //dd($order_array);
+        $month_order_array = array_values($month_unique);
+
+        $month_array = array_unique($month_array);
+
+        $month_array = array_values($month_array);
+
+        /* Conversione in json_encode */
+        $year_array = json_encode($year_array, JSON_NUMERIC_CHECK);
+        $year_order_array = json_encode($year_order_array, JSON_NUMERIC_CHECK);
+        $month_array = json_encode($month_array, JSON_NUMERIC_CHECK);
+        $month_order_array = json_encode($month_order_array, JSON_NUMERIC_CHECK);
        
         /* Data */
         $data = [
-            'orders' => $order_array,
+            'year' => $year_array,
+            'year_order' => $year_order_array,
+            'month' => $month_array,
+            'month_order' => $month_order_array
         ];
 
-        return view('admin.stats', compact('data'));
-    }
-
-    public function sortByYear($array){
-
-        dump($array);
-
+        return view('admin.stats', $data);
     }
 }
