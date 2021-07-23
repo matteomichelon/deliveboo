081591f862e265308b0c62b5219a2fa1fa21434f
Matteo Michelon <michelon.matteo89@gmail.com>
2021-07-23 16:07:37 +0200

    modificato controller statistiche

--- app/Http/Controllers/User/StatsController.php
+++ app/Http/Controllers/User/StatsController.php
@@ -26,30 +26,63 @@ class StatsController extends Controller
 
         $array = collect($order_schema)->where('id', $id)->all();
 
+        $year_array = [];
+
+
+        $year_order_array = [];
+        $month_array = [
+            'gennaio',
+            'febbraio',
+            'marzo',
+            'aprile',
+            'maggio',
+            'giugno',
+            'luglio',
+            'agosto',
+            'settembre',
+            'ottobre',
+            'novembre',
+            'dicembre'
+        ];
+        $month_order_array = [];
+
         /* foreach ($array as $value) {
             dump($value->date);
         } */
                 
         /* Array suddiviso per anni e mesi */
-        $data_array = [];
-
         foreach ($array as $value) {
             $date = $value->date;
             $year_date = '';
             $month_date = '';
-            $id_order =  $value->order_id;
-            
+            $id_order =  $value->order_id;            
             
             if ($date) {
                 $year_date = date('Y', strtotime($date));
-                $month_date = date('F', strtotime($date));
-
-                array_push($data_array,$year_date,$month_date);
+                //$month_date = date('F', strtotime($date));
+                array_push($year_array,$year_date);
+                //array_push($data_array, $year_date, $month_date);
             }
         }
 
-        $order_array = array_count_values($data_array);
-       /*  dd($order_array);*/
+        usort($year_array, fn($a, $b) => strtotime($a) - strtotime($b));
+
+        /* TODO: contare ordini per anno */
+        $year_unique = array_count_values($year_array);
+
+        $year_order_array = array_values($year_unique);
+
+        dump($year_order_array);
+
+        $year_array = array_unique($year_array);
+       
+
+        dd($year_array);
+
+        
+
+        
+        //dd($order_array);
        
         /* Data */
         $data = [
@@ -57,5 +90,11 @@ class StatsController extends Controller
         ];
 
         return view('admin.stats', compact('data'));
+    }
+
+    public function sortByYear($array){
+
+        dump($array);
+
     }
 }
