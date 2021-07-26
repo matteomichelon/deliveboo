83dd334224261f86bdcc0e2ec702312be8361e5e
Matteo Michelon <michelon.matteo89@gmail.com>
2021-07-26 16:27:00 +0200

    aggiornato order controller e blade di ordini

--- app/Http/Controllers/User/OrderController.php
+++ app/Http/Controllers/User/OrderController.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Facades\Auth;
 use App\Order;
 use App\User;
 
-
 class OrderController extends Controller
 {
     public function index()
@@ -18,20 +17,35 @@ class OrderController extends Controller
 
         /* Join Sql per gli ricavare Utenti/Ordini */
         $order_schema = DB::table('order_product')
-                    ->join('products', 'product_id', '=', 'products.id')                    
-                    ->join('users', 'user_id', '=', 'users.id')
+                    ->select('users.id', 'orders.code', 'orders.name', 'orders.surname', 'orders.date', 'orders.price')
+                    ->join('products', 'product_id', '=', 'products.id')
                     ->join('orders', 'order_id', '=', 'orders.id')
+                    ->join('users', 'user_id', '=', 'users.id')
+                    ->orderByDesc('date')
                     ->get();
 
-
         $orders = collect($order_schema)->where('id', $id)->all();
-      
 
+
+        /*   $array_order=[];
+
+          foreach ($orders as $order) { */
+        /* if (property_exists($order, 'code')) {
+            array_push($array_order, $order);
+            dump($order->code);
+        } */
+        /*    if (in_array($order->code, $array_order)) {
+
+               array_push( $order);
+           }
+        }
+*/
+ 
         $data =  [
             'orders' => $orders
         ];
 
         return view('admin.orders', $data);
-
     }
+
 }
--- resources/views/admin/orders.blade.php
+++ resources/views/admin/orders.blade.php
@@ -2,14 +2,27 @@
 
 @section('content')
 
-
-    @foreach ($orders as $order)
-        <div class="row">
-            <div class="d-flex justify-content-between align-items-center">
-                <div> {{ $order->code }} </div>
-            </div>
-        </div>
-    @endforeach
-
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Codice</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Cognome</th>
+                <th scope="col">Data</th>
+                <th scope="col">Prezzo</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($orders as $order)
+                <tr>
+                    <th scope="row">{{ $order->code }}</th>
+                    <td>{{ $order->name }}</td>
+                    <td>{{ $order->surname }}</td>
+                    <td>{{ $order->date }}</td>
+                    <td>{{ $order->price }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 
 @endsection
